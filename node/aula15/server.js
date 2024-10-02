8f41121929acf9901ede615ba5e431986a20c0f9
Alexandre <cardoal01@gmail.com>
2024-10-02 09:37:36 -0300

    feat: Add csurf and helmet imports to server.js
    This commit adds the necessary imports for the csurf and helmet packages to the server.js file. The csurf package is used for CSRF protection, while the helmet package adds security headers to the application.

--- node/aula15/server.js
+++ node/aula15/server.js
@@ -15,15 +15,19 @@ const flash = require('connect-flash');
 
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOption = session({
   secret: 'kfidfjiufjcksd ffijifuidjjAAvx86755$#%$gf',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
@@ -38,8 +42,12 @@ app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
+
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('ok', () => {
