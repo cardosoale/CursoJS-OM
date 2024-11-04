fac7c344ff10e4eae73feff224fd1890d41a85e5
Alexandre <cardoal01@gmail.com>
2024-11-04 13:19:15 -0300

    feat: Implement login middleware and update UserController and routes for authentication

--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
+// Nao deveria existir
+// router.get('/', loginRequired, userController.index); // Lista Usuários
+// router.get('/:id', userController.show); // Lista Usuário
+
 router.post('/', userController.store);
-router.get('/', userController.index);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 export default router;
