5ab71500d2901213545c17e6ebb42781224be47b
Alexandre <cardoal01@gmail.com>
2024-11-13 16:10:22 -0300

    feat: Enhance Aluno model and controller to include associated Foto data, add appConfig for URL management, and serve static uploads

--- api_rest/src/controllers/AlunoController.js
+++ api_rest/src/controllers/AlunoController.js
@@ -1,9 +1,27 @@
 import Aluno from '../models/Aluno';
-// import Foto from '../models/Foto';
+import Foto from '../models/Foto';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
+    const alunos = await Aluno.findAll({
+      attributes: [
+        'id',
+        'nome',
+        'sobrenome',
+        'email',
+        'idade',
+        'peso',
+        'altura',
+      ],
+      order: [
+        ['id', 'DESC'],
+        [Foto, 'id', 'DESC'],
+      ],
+      include: {
+        model: Foto,
+        attributes: ['filename', 'url'],
+      },
+    });
     res.json(alunos);
   }
 
@@ -29,7 +47,25 @@ class AlunoController {
         });
       }
 
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: [
+          'id',
+          'nome',
+          'sobrenome',
+          'email',
+          'idade',
+          'peso',
+          'altura',
+        ],
+        order: [
+          ['id', 'DESC'],
+          [Foto, 'id', 'DESC'],
+        ],
+        include: {
+          model: Foto,
+          attributes: ['filename', 'url'],
+        },
+      });
 
       if (!aluno) {
         return res.status(400).json({
