0bc0aed804267cf76dfff4608249f6c30096f2b7
Alexandre <cardoal01@gmail.com>
2024-11-19 09:59:00 -0300

    feat: Add react-icons for task management and enhance Main component with new styles and functionality

--- REACT/listatarefas/src/components/Main.js
+++ REACT/listatarefas/src/components/Main.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+// Form
+import { FaPlus } from "react-icons/fa";
+
+// Tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 import "./Main.css";
 
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Fazer café", "Beber agua", "Estudar"],
   };
 
   handleChange = (e) => {
@@ -14,16 +21,29 @@ export default class Main extends Component {
   };
 
   render() {
-    // const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input type="text" onChange={this.handleChange} />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input type="text" onChange={this.handleChange} value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa.id}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
