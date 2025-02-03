348aaae3128d10ddfe5c7997af8a6a72c38140f8
Alexandre Cardoso <cardoal01@gmail.com>
2025-02-03 12:27:40 -0300

    feat: Refactor state management for button click and update Login component styling

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import { Title } from './styled';
+import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyles';
+import * as exampleActions from '../../store/modules/example/action';
 
 export default function Login() {
   const dispatch = useDispatch();
@@ -10,15 +11,13 @@ export default function Login() {
   function handleClick(e) {
     e.preventDefault();
 
-    dispatch({
-      type: 'BOTAO_CLICADO',
-    });
+    dispatch(exampleActions.clicaBotao());
   }
   return (
     <Container>
       <Title isRed={false}>Test component</Title>
       <small>Oii</small>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
       <button type="button" onClick={handleClick}>
         Enviar
       </button>
