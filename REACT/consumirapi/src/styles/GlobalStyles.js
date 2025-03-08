975eec3d50aedd98987fa0847dba4c90a65b28e9
Alexandre Cardoso <cardoal01@gmail.com>
2025-03-08 11:52:02 -0300

    feat: Refactor GlobalStyles to use color constants and improve button hover effect

--- REACT/consumirapi/src/styles/GlobalStyles.js
+++ REACT/consumirapi/src/styles/GlobalStyles.js
@@ -1,55 +1,60 @@
 import styled, { createGlobalStyle } from 'styled-components';
-
-import { primaryDarkColor, primaryColor } from '../config/colors';
-
-import 'react-toastify/ReactToastify.css';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  outline: none;
-  box-sizing: border-box;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
 
-body {
-  font-family: sans-serif;
-  background: ${primaryDarkColor};
-  color:  ${primaryDarkColor};
-}
+  body {
+    font-family: sans-serif;
+    background: ${colors.primaryDarkColor};
+    color: ${colors.primaryDarkColor};
+  }
 
-html, body, #root {
-  height: 100%;
-}
+  html, body, #root {
+    height: 100%;
+  }
 
-button {
-  cursor: pointer;
-  background: ${primaryColor};
-  border: none;
-  color: #fff;
-  padding: 10px 20px;
-  border-radius: 4px;
-  font-weight: 700;
-  transition: all 300ms;
-}
+  button {
+    cursor: pointer;
+    background: ${colors.primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
+    transition: all 300ms;
+  }
 
-button:hover {
-  filter: brightness(85%)
-}
+  button:hover {
+    filter: brightness(75%);
+  }
 
-a {
-  background: ${primaryColor};
-  text-decoration: none;
-}
+  a {
+    text-decoration: none;
+    color: ${colors.primaryColor};
+  }
 
-ul {
-  list-style: none;
-}
+  ul {
+    list-style: none;
+  }
 
+  body .Toastify .Toastify__toast-container .Toastify__toast--success {
+    background: ${colors.successColor}
+  }
+
+  body .Toastify .Toastify__toast-container .Toastify__toast--error {
+    background: ${colors.errorColor}
+  }
 `;
 
 export const Container = styled.section`
-  max-width: 360px;
+  max-width: 480px;
   background: #fff;
   margin: 30px auto;
   padding: 30px;
