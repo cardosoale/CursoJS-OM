10eb09c5694a82259d5ac14623cee61ace9a98b5
Alexandre Cardoso <cardoal01@gmail.com>
2025-03-13 12:29:37 -0300

    feat: Implement form for adding and editing student details with validation

--- REACT/consumirapi/src/pages/Aluno/index.js
+++ REACT/consumirapi/src/pages/Aluno/index.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { Form } from './styled';
 
 import { Container } from '../../styles/GlobalStyles';
+import { toast } from 'react-toastify';
 
-export default function Aluno() {
+export default function Aluno({ match }) {
+  const id = get(match, 'params.id', '');
+  const [nome, setNome] = React.useState('');
+  const [sobrenome, setSobrenome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [idade, setIdade] = React.useState('');
+  const [peso, setPeso] = React.useState('');
+  const [altura, setAltura] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (nome.length < 3 && nome.length > 255) {
+      toast.error('Nome precisa ter pelo menos 3 caracteres');
+    }
+    console.log('nome', nome);
+  };
+
   return (
     <Container>
-      <h1>Aluno</h1>
+      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder="Nome"
+        />
+        <input
+          type="text"
+          value={sobrenome}
+          onChange={(e) => setSobrenome(e.target.value)}
+          placeholder="Sobrenome"
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="number"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          placeholder="Idade"
+        />
+        <input
+          type="text"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+          placeholder="Peso"
+        />
+        <input
+          type="text"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+          placeholder="Altura"
+        />
+        <button type="submit">Enviar</button>
+      </Form>
     </Container>
   );
 }
+
+Aluno.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
