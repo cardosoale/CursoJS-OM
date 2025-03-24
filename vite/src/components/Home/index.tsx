75f9835a68ef239b8dad14ea7643f712ca097d2a
Alexandre Cardoso <cardoal01@gmail.com>
2025-03-24 13:22:42 -0300

    feat: Add state display in Home component and clean up Menu component

--- vite/src/components/Home/index.tsx
+++ vite/src/components/Home/index.tsx
@@ -1,9 +1,12 @@
+import { useLocation } from 'react-router-dom';
 import './style.css'
 
 export const Home = () => {
+  const { state } = useLocation();
   return (
     <div>
       <h1>Home</h1>
+      <p>{state}</p>
     </div>
   );
 }
--- vite/src/components/Menu/index.tsx
+++ vite/src/components/Menu/index.tsx
@@ -2,6 +2,7 @@ import './style.css'
 import { Link } from 'react-router-dom'
 
 export const Menu = () => {
+
   return (
     <nav className="menu">
     <ul>
